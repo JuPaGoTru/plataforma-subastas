5af3674f35bf6cd425f8136d124aea6403fbd8c1
JuPaGoTru <jupagotru97@gmail.com>
2025-11-22 14:10:12 -0500

    Refactorizar la configuración para usar python-decouple para variables de entorno

--- auction_site/auction_site/settings.py
+++ auction_site/auction_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9i)jm5_b0szf$&n&g!v_s&jl7wj+m5z2+)lb+%ksorhq46ktku'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".ngrok.app"]
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Permitir CSRF desde ngrok
 CSRF_TRUSTED_ORIGINS = [
